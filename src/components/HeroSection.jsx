1be0dd7092655fa736245f85e01d6413fbe9f12d
Ayo-Adedeji <dasammayo@gmail.com>
2025-08-12 10:33:44 +0100

    new commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.jpg";
 import img4 from "../assets/img4.jpg";
 import { BsCalendar2DateFill } from "react-icons/bs";
-import { FaLocationPinLock, FaMapLocation } from "react-icons/fa6";
+import { FaMapLocation } from "react-icons/fa6";
 
 const images = [img1, img2, img3, img4];
 
